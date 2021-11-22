f07e4785c867d868776735870d3cf0c801e81f74
unknown <gissaud.alexandre@gmail.com>
2021-11-22 16:21:57 +0100

    benchmark version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,7 +10,8 @@ import tensorflow as tf
 print(f'TensorFlow version: {tf.__version__}')
 print(f'Keras version: {tf.keras.__version__}')
 
-
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from tensorflow import keras # uniquement pour Input, faudrait, genre, import que ça
@@ -17,7 +19,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.utils import to_categorical
 
-
+'''
 def plot_loss_acc(history):
     """Plot training and (optionally) validation loss and accuracy"""
 
@@ -54,51 +56,84 @@ def plot_loss_acc(history):
         title += ', Validation accuracy: {:.2f}%'.format(final_val_acc * 100)
     plt.title(title)
     plt.legend()
+'''
 
 csv_x_train_url = "train_X.csv"
 csv_y_train_url = "train_Y.csv"
-#csv_x_test_url = "test_X.csv"
+csv_x_test_url = "test_X.csv"
 
 df_x_train = pd.read_csv(csv_x_train_url).drop("ChallengeID", axis=1)
 df_y_train = pd.read_csv(csv_y_train_url).drop("ChallengeID", axis=1)
+df_x_test = pd.read_csv(csv_x_test_url)
 
 x = df_x_train.to_numpy()
 y = df_y_train.to_numpy()
+x_test = df_x_test.to_numpy()
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size=0.25)
 
+print(f"x_train: {x_train.shape}")
+print(f"y_train: {y_train.shape}")
+
+'''
 scaler = StandardScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
-# y_train = to_categorical(y_train)
-print(f"x_train: {x_train.shape}")
-print(f"y_train: {y_train.shape}")
-
 model = Sequential()
 model.add(keras.Input(shape=x.shape[1]))
-model.add(Dense(200, activation="sigmoid"))
-model.add(Dense(180, activation="sigmoid"))
-model.add(Dense(220, activation="sigmoid"))
-model.add(Dense(y.shape[1], activation="relu"))
+model.add(Dense(300, activation="relu"))
+model.add(Dense(280, activation="relu"))
+model.add(Dense(260, activation="relu"))
+model.add(Dense(y.shape[1], activation="sigmoid"))
 
 model.summary()
 
-# opt = keras.optimizers.RMSprop(learning_rate=0.001, centered=True)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+opt = keras.optimizers.RMSprop(learning_rate=0.001, centered=True)
 
-history = model.fit(x_train, y_train, epochs=15, batch_size=128)
+history = model.fit(x_train, y_train, epochs=10, batch_size=128)
 
 plot_loss_acc(history)
 
-val = x_train[0]
-res = model.predict(x_train)
-print(f"resultat: {res[0]}")
-tab = np.copy(res[0])
-for i in range(len(res[0])):
-    tab[i] = 0 if res[0][i] < 0.15 else 1
+res = model.predict(x_test)
+'''
 
-print(f"rounded up: {tab}")
+def variable_threshold(input, output):
+    threshold_tab = numpy.empty(248)
+    threshold_tab[:] = 0.5
 
-plt.show()
+    for n in range(output.shape[0]):
+        for col in range(output.shape[1]):
+            if (input[n, col] > threshold_tab[col] and output[n, col] == 0) or \
+                    (input[n, col] <= threshold_tab[col] and output[n, col] == 1):
+                threshold_tab[col] = (threshold_tab[col]*(n+1) + input[n, col])/(n+2)
+    return threshold_tab
+
+def threshold_decision(input, v_threshold):
+    input = input[:,:248]
+    output = numpy.empty(shape=(input.shape[0], 248), dtype="uint8")
+    for i, x in np.ndenumerate(input):
+        output[i[0], i[1]] = 0 if x < v_threshold[i[1]] else 1
+    return output
+
+#threshold_decision = np.vectorize(lambda t : (lambda x : 0 if x < t[i] else 1 for i in range(t.shape[0])))
+
+# decision_mat = threshold_decision(x_test[:,1:], variable_threshold(x_train, y_train))
+# x_test[:,1:249] = decision_mat[:,:]
+
+decision_mat = threshold_decision(x_validation, variable_threshold(x_train, y_train))
+accuracy = accuracy_score(decision_mat, y_validation, normalize=False)
+
+# FOUTRE DANS LE CSVVVVV
+
+df_y_train.data[:decision_mat.shape[0],1:249] = decision_mat
+
+# df_x_test.drop(:,249:)
+# df_x_test.Data[:,1:249] = decision_mat
+
+
+print(f"Accuracy : {accuracy}")
+
+numpy.savetxt("y_test.csv", x, delimiter=',')
